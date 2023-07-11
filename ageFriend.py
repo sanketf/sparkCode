1d91fda83dae284f4b6889054a330beafe3ab7ac
sanketf <sanket.fafat@viit.ac.in>
2023-07-11 23:56:24 +0530

    changed the data path.

--- ageFriend.py
+++ ageFriend.py
@@ -1,20 +1,22 @@
 from pyspark import SparkConf, SparkContext
 
+
 def parseLine(line):
     fields = line.split(",")
     age = int(fields[2])
     friends = int(fields[3])
-    return(age, friends)
+    return age, friends
+
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
-lines = sc.textFile("file:///sparkCourse/data/fakefriends.csv");
+lines = sc.textFile("file:///sparkCourse/data/fakefriends.csv")
 rdd = lines.map(parseLine)
 
-totalByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0]+y[0], x[1]+y[1]))
+totalByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
 
-averageByAge = totalByAge.mapValues(lambda x: x[0]/x[1])
+averageByAge = totalByAge.mapValues(lambda x: x[0] / x[1])
 
 result = averageByAge.collect()
 
