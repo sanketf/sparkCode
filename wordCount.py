bc8c285633a519f27529aef5d9680dc8d0e79883
sanketf <sanket.fafat@viit.ac.in>
2023-07-15 22:52:24 +0530

    add new file

--- wordCount.py
+++ wordCount.py
@@ -1,13 +1,24 @@
 from pyspark import SparkConf, SparkContext
+import re
+
+def normilizeWord(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
 
 conf = SparkConf().setMaster("local").setAppName("Wordcount")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
-input = sc.textFile("data/book.txt")
-words = input.flatMap(lambda x: x.split())
-wordCount = words.countByValue()
+inputs = sc.textFile("data/book.txt")
+words = inputs.flatMap(normilizeWord)
+# wordCount = words.countByValue()
 
-for word, count in wordCount.items():
-    cleanWord = word.encode('ascii', 'ignore')
-    if(cleanWord):
-        print(cleanWord, count)
+wordCount = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+#sortWords = wordCount.map(lambda x,y: (y,x))
+sortedWordCount = wordCount.sortByKey()
+results = sortedWordCount.collect()
+
+for result in results:
+    count = result[1]
+    word = result[0].encode('ascii', 'ignore')
+    if word:
+        print(word, ": ", count)
